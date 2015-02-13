060aafeeb57249fe51b1c686322abb3ed99d2de3
jainniket <niketjain@rocketmail.com>
2015-02-13 17:39:58 +0530

    adding flex

--- js/index.js
+++ js/index.js
@@ -33,22 +33,12 @@ var cities = [{
 
 
 //this function create each <li> node
-<<<<<<< HEAD
-var createListNode = function (city) {
-
-   // we will simply return is object if item is not correct
-   var failResponse = {
-       success: false,
-       message: '| ' + JSON.stringify(city) + ' | is not a valid item, hence skipping!'
-   };
-=======
 var createListNode = function(city) {
     // we will simply return is object if item is not correct
     var failResponse = {
         success: false,
         message: '| ' + JSON.stringify(city) + ' | is not a valid item, hence skipping!'
     };
->>>>>>> 79cc8e99ec6f8f2fe4b9b5465786c7b0c1c0a7cb
 
     // cheching for correct data  type
     if (city !== null && typeof city === 'object') {
@@ -60,23 +50,6 @@ var createListNode = function(city) {
                 nameSpan = document.createElement('span'),
                 cityName = document.createTextNode(city.name);
 
-<<<<<<< HEAD
-           li.appendChild(image);
-           nameSpan.appendChild(cityName);
-           li.appendChild(nameSpan);
-           
-           return {
-               success: true,
-               node: li
-           }
-       } else {
-           return failResponse;
-       }
-   } else {
-       return failResponse;
-   }
-
-=======
             li.setAttribute('id', city.name);
             image.setAttribute('src', city.url);
 
@@ -94,28 +67,18 @@ var createListNode = function(city) {
     } else {
         return failResponse;
     }
->>>>>>> 79cc8e99ec6f8f2fe4b9b5465786c7b0c1c0a7cb
 };
 
 var docFrag = document.createDocumentFragment();
 
 for (var i = 0, len = cities.length; i < len; i++) {
-<<<<<<< HEAD
 
-   var li = createListNode(cities[i]);
-   if (!li.success) {
-       console.info(li.message);
-   } else {
-       docFrag.appendChild(li.node);
-   }
-=======
     var li = createListNode(cities[i]);
     if (!li.success) {
         console.info(li.message);
     } else {
         docFrag.appendChild(li.node);
     }
->>>>>>> 79cc8e99ec6f8f2fe4b9b5465786c7b0c1c0a7cb
 }
 
 var listNode = document.getElementById('city');
@@ -129,9 +92,3 @@ for (var i = 0; i < lis.length; i++) {
 function redirect() {
     window.location = 'details.html?city=' + this.id;
 }
-<<<<<<< HEAD
-function redirect(){
-    window.location='details.html?city=' + this.id;
-}
-=======
->>>>>>> 79cc8e99ec6f8f2fe4b9b5465786c7b0c1c0a7cb
