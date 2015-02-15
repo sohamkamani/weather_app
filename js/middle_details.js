91f9e7071847a9ee1c4dc1262dc58010942280a6
Soham Chetan <sohamchetan@Sohams-MacBook-Pro.local>
2015-02-15 23:48:43 +0530

    made the additional details , fixed the middle section temperature conversion problem and improved icon sizing

--- js/middle_details.js
+++ js/middle_details.js
@@ -38,7 +38,10 @@ function show() {
   var weather_picture = document.getElementById("weather-picture")
   var cityNameDisplay = document.getElementById("city-name")
 
-
+  var windChillDisplay = document.getElementById("wind-chill-display")
+  var visibilityDisplay = document.getElementById("visibility-display")
+  var windDirectionDisplay = document.getElementById("wind-direction-display")
+  var pressureDisplay = document.getElementById("pressure-display")
 
   getJSON(
     "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" +
@@ -61,6 +64,10 @@ function show() {
     var sunsetTime = data['astronomy']['sunset']
     var windSpeed = data['wind']['speed'] + data['units']['speed']
     var humidity = data['atmosphere']['humidity'] + "%"
+    var windChill = farToCel(data['wind']['chill']) + "°C"
+    var visibility = data['atmosphere']['visibility']
+    var windDirection = data['wind']['direction']
+    var pressure = data['atmosphere']['pressure'] + data['units']['pressure']
     var cityName = data['location']['city'] + ", " + data['location']['country'];
 
     temperatureDisplay.replaceChild(document.createTextNode(temperature), temperatureDisplay.childNodes[0])
@@ -70,6 +77,10 @@ function show() {
     windDisplay.replaceChild(document.createTextNode(windSpeed), windDisplay.childNodes[0])
     humidityDisplay.replaceChild(document.createTextNode(humidity), humidityDisplay.childNodes[0])
     cityNameDisplay.replaceChild(document.createTextNode(cityName), cityNameDisplay.childNodes[0])
+    windChillDisplay.replaceChild(document.createTextNode(windChill), windChillDisplay.childNodes[0])
+    visibilityDisplay.replaceChild(document.createTextNode(visibility), visibilityDisplay.childNodes[0])
+    windDirectionDisplay.replaceChild(document.createTextNode(windDirection), windDirectionDisplay.childNodes[0])
+    pressureDisplay.replaceChild(document.createTextNode(pressure), pressureDisplay.childNodes[0])
     weather_picture.setAttribute("src", getThemeFor(weatherStatus.toLowerCase())[
       1]);
     setColorTheme(getThemeFor(weatherStatus.toLowerCase())[2]);
@@ -118,37 +129,37 @@ function celToFar(c) {
 
 
 function getThemeFor(weather_text) {
-    var colorArray={
-                        "sunny":["#FFF9C4", "#FFEE58", "#FFF176", "#FFF59D"],
-                        
-                        "clear":["#C8E6C9", "#66BB6A", "#81C784", "#A5D6A7"],
+  var colorArray = {
+    "sunny": ["#FFF9C4", "#FFEE58", "#FFF176", "#FFF59D"],
 
-                        "cloud":["#F5F5F5", "#BDBDBD", "#E0E0E0", "#EEEEEE"],
-                        
-                        "fog":["#f0f4c3", "#d4e157", "#dce775", "#e6ee9c"],
-                        
-                        "rainy":["#B2EBF2", "#26C6DA", "#4DD0E1", "#80DEEA"],
-                        
-                        "snow":["#E8EAF6", "#7986CB", "#9FA8DA", "#C5CAE9"],
-                        
-                        "windy":["#DCEDC8", "#9CCC65", "#AED581", "#C5E1A5"]
-                        };
-    if (weather_text.indexOf('sun') > -1 || weather_text.indexOf('hot') > -1) {
-        return ["sunny","SVG/Sun.svg",colorArray["sunny"]]
-    } else if (weather_text.indexOf('rain') > -1) {
-        return ["rainy","SVG/Cloud-Rain.svg",colorArray["rainy"]]
-    } else if (weather_text.indexOf('snow') > -1 || weather_text.indexOf('ice') > -1) {
-        return ["snow","SVG/Cloud-Snow.svg",colorArray["snow"]]
-    } else if (weather_text.indexOf('fog') > -1 || weather_text.indexOf('haze') > -1) {
-        return ["fog","SVG/Cloud-Fog-Alt.svg",colorArray["fog"]]
+    "clear": ["#C8E6C9", "#66BB6A", "#81C784", "#A5D6A7"],
 
-    } else if (weather_text.indexOf('fair') > -1 || weather_text.indexOf('clear') > -1) {
-        return ["clear","SVG/Sun-Low.svg",colorArray["clear"]]
-    } else if (weather_text.indexOf('wind') > -1) {
-        return ["windy","SVG/Cloud-Wind.svg",colorArray["windy"]]
-    } else {
-        return ["cloud","SVG/Cloud.svg",colorArray["cloud"]]
-    }
+    "cloud": ["#F5F5F5", "#BDBDBD", "#E0E0E0", "#EEEEEE"],
+
+    "fog": ["#f0f4c3", "#d4e157", "#dce775", "#e6ee9c"],
+
+    "rainy": ["#B2EBF2", "#26C6DA", "#4DD0E1", "#80DEEA"],
+
+    "snow": ["#E8EAF6", "#7986CB", "#9FA8DA", "#C5CAE9"],
+
+    "windy": ["#DCEDC8", "#9CCC65", "#AED581", "#C5E1A5"]
+  };
+  if (weather_text.indexOf('sun') > -1 || weather_text.indexOf('hot') > -1) {
+    return ["sunny", "SVG/Sun.svg", colorArray["sunny"]]
+  } else if (weather_text.indexOf('rain') > -1) {
+    return ["rainy", "SVG/Cloud-Rain.svg", colorArray["rainy"]]
+  } else if (weather_text.indexOf('snow') > -1 || weather_text.indexOf('ice') > -1) {
+    return ["snow", "SVG/Cloud-Snow.svg", colorArray["snow"]]
+  } else if (weather_text.indexOf('fog') > -1 || weather_text.indexOf('haze') > -1) {
+    return ["fog", "SVG/Cloud-Fog-Alt.svg", colorArray["fog"]]
+
+  } else if (weather_text.indexOf('fair') > -1 || weather_text.indexOf('clear') > -1) {
+    return ["clear", "SVG/Sun-Low.svg", colorArray["clear"]]
+  } else if (weather_text.indexOf('wind') > -1) {
+    return ["windy", "SVG/Cloud-Wind.svg", colorArray["windy"]]
+  } else {
+    return ["cloud", "SVG/Cloud.svg", colorArray["cloud"]]
+  }
 }
 
 function callalert() {
@@ -168,11 +179,15 @@ function init() {
   var far = document.getElementById("deg-f")
   var header = document.getElementById("header")
   var middleSection = document.getElementById("middle-info")
+  var weatherIcon = document.getElementById("weather-icon")
+  var infoSection = document.getElementById("info-section")
   var footer = document.getElementById("footer")
+  var hiddenList = document.getElementsByClassName("hidden-list")
   cel.onclick = null;
   far.onclick = farClick
   header.onclick = headerAnimationGoDown
-  middleSection.onclick = middleAnimationComeOut
+  weatherIcon.onclick = middleAnimationComeOut
+  infoSection.onclick = middleAnimationComeOut
 
   function farClick() {
     newTemperature = document.createTextNode(celToFar(temperature.innerHTML))
@@ -199,8 +214,12 @@ function init() {
     header.classList.add("go-full")
     middleSection.classList.add("go-down")
     footer.classList.add("go-down")
+    for (var i = 0; i < hiddenList.length; i++) {
+      hiddenList[i].classList.remove("make-block")
+    }
     header.onclick = headerAnimationGoBackUp
-    middleSection.onclick = middleAnimationComeOut
+    weatherIcon.onclick = middleAnimationComeOut
+    infoSection.onclick = middleAnimationComeOut
 
   }
 
@@ -215,13 +234,21 @@ function init() {
     footer.classList.add("go-to-20")
     middleSection.classList.add("go-to-60")
     header.classList.add("go-to-20")
-    middleSection.onclick = middleAnimationComeBack
+    for (var i = 0; i < hiddenList.length; i++) {
+      hiddenList[i].classList.add("make-block")
+    }
+    weatherIcon.onclick = middleAnimationComeBack
+    infoSection.onclick = middleAnimationComeBack
   }
 
   function middleAnimationComeBack() {
     footer.classList.remove("go-to-20")
     middleSection.classList.remove("go-to-60")
     header.classList.remove("go-to-20")
-    middleSection.onclick = middleAnimationComeOut
+    for (var i = 0; i < hiddenList.length; i++) {
+      hiddenList[i].classList.remove("make-block")
+    }
+    weatherIcon.onclick = middleAnimationComeOut
+    infoSection.onclick = middleAnimationComeOut
   }
 }
