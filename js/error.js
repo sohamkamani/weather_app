21fac090fc280802434789e6ae3d7d666d0ff0a1
Mahesh Haldar <haldar.mahesh@gmail.com>
2015-02-16 14:48:34 +0530

    added the viewport to error html file

--- js/error.js
+++ js/error.js
@@ -1,12 +1,10 @@
-function redirect()
-{
-  var city = document.getElementById("city").value;
-  if(city.length < 1)
-    {
-      alert("Please enter something");
-      return false;
-    
+function redirect() {
+    var city = document.getElementById("city").value;
+    if (city.length < 1) {
+        alert("Please enter something");
+        return false;
+
     }
-  window.location = 'details.html?city=' + city;
+    window.location = 'details.html?city=' + city;
 
 }
