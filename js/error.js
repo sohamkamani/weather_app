26147bd752613d3370d917c3c4b2fe1a95ce666f
Mahesh Haldar <haldar.mahesh@gmail.com>
2015-02-16 15:00:35 +0530

    added the home button in error page

--- js/error.js
+++ js/error.js
@@ -1,10 +1,13 @@
-function redirect() {
-    var city = document.getElementById("city").value;
-    if (city.length < 1) {
-        alert("Please enter something");
-        return false;
-
+function redirect()
+{
+  var city = document.getElementById("city").value;
+  if(city.length < 1)
+    {
+      alert("Please enter something");
+      return false;
+    
     }
-    window.location = 'details.html?city=' + city;
+  window.location = 'details.html?city=' + city;
 
 }
+
